81853f54e841ef9637497c57b7e9ed70bd476037
mgebhardt <mgebhardt@734bee23-065b-40fc-b63d-96990fe7ffae>
2011-08-14 17:53:15 +0000

    Added 3.0.sql to sql update section. Tried to fix installation bugs of version 3.0.
    git-svn-id: http://impressumreload.svn.sourceforge.net/svnroot/impressumreload@17 734bee23-065b-40fc-b63d-96990fe7ffae

--- script.php
+++ script.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version		3.0 $Id$
+ * @version		3.0.1 $Id$
  * @package		Joomla
  * @subpackage	Impressum
  * @copyright	(C) 2011 Mathias Gebhardt
@@ -31,6 +31,15 @@ defined('_JEXEC') or die('Restricted access');
  */
 class com_impressumInstallerScript
 {
+	/**
+	 * Current version.
+	 * 
+	 * @author	mgebhardt
+	 * @var		string
+	 * @since	3.0.1
+	 */
+	private $release = '3.0.1';
+	
 	/**
 	 * Method to install the component.
 	 * 
@@ -78,13 +87,118 @@ class com_impressumInstallerScript
 	 * @author	mgebhardt
 	 * @param	object	$parent	Is the class calling this method.
 	 * @param	string	$type	Is the type of change (install, update or discover_install).
-	 * @since	3.0
+	 * @since	3.0.1
 	 */
-	// Not used.
-// 	function preflight($type, $parent) 
-// 	{
-// 		echo '<p>' . JText::_('COM_IMPRESSUM_PREFLIGHT_' . $type . '_TEXT') . '</p>';
-// 	}
+	function preflight($type, $parent) 
+	{
+		// this component does not work with Joomla releases prior to 1.6
+		// abort if the current Joomla release is older
+		$jversion = new JVersion();
+		if( version_compare( $jversion->getShortVersion(), '1.6', 'lt' ) )
+		{
+			Jerror::raiseWarning(null, 'COM_IMPRESSUM_PREFLIGHT_ERROR_JOOMLA_VERSION');
+			//Cannot install com_impressum in a Joomla release prior to 1.6
+			return false;
+		}
+		
+		// abort if the release being installed is not newer than the currently installed version
+		if ( $type == 'update' )
+		{
+			$oldVersion = $this->getOldVersion();
+			$rel = $oldVersion . ' to ' . $this->release;
+			if ( version_compare( $this->release, $oldVersion, 'le' ) )
+			{
+				Jerror::raiseWarning(null, 'COM_IMPRESSUM_PREFLIGHT_ERROR_IMPRESSUM_VERSION' . $rel);
+				//Incorrect version sequence. Cannot upgrade 
+				return false;
+			}
+			// Fixes the installation bugs of version 3.0 (wrong langeguage directory and missing 3.0.sql).
+
+			// SQL query does not work. Deleting files work.
+			
+			/* if ($oldVersion == '3.0')
+			{
+				jimport('joomla.filesystem.file');
+	
+				// Get the posted config options.
+				$vars = JRequest::getVar('jform', array());
+			
+				$path = JPATH_ADMINISTRATOR;
+			
+				// check whether we need to use FTP
+				$useFTP = false;
+				if ((file_exists($path) && !is_writable($path))) {
+					$useFTP = true;
+				}
+			
+				// Check for safe mode
+				if (ini_get('safe_mode')) {
+					$useFTP = true;
+				}
+			
+				// Enable/Disable override
+				if (!isset($options->ftpEnable) || ($options->ftpEnable != 1)) {
+					$useFTP = false;
+				}
+			
+				if ($useFTP == true) {
+					// Connect the FTP client
+					jimport('joomla.client.ftp');
+					jimport('joomla.filesystem.path');
+			
+					$ftp = JFTP::getInstance($options->ftp_host, $options->ftp_port);
+					$ftp->login($options->ftp_user, $options->ftp_pass);
+			
+					// Translate path for the FTP account
+					// @TODO: check whether files exists or not
+					$file = JPath::clean(str_replace(JPATH_CONFIGURATION, $options->ftp_root, $path), '/language/de-DE/de-DE.com_impressum.ini');
+					$return = $ftp->delete($file);
+					$file = JPath::clean(str_replace(JPATH_CONFIGURATION, $options->ftp_root, $path), '/language/de-DE/de-DE.com_impressum.sys.ini');
+					$return = $return && $ftp->delete($file);
+					
+					$file = JPath::clean(str_replace(JPATH_CONFIGURATION, $options->ftp_root, $path), '/language/en-GB/en-GB.com_impressum.ini');
+					$return = $return && $ftp->delete($file);
+					$file = JPath::clean(str_replace(JPATH_CONFIGURATION, $options->ftp_root, $path), '/language/en-GB/en-GB.com_impressum.sys.ini');
+					$return = $return && $ftp->delete($file);
+			
+					$file = JPath::clean(str_replace(JPATH_CONFIGURATION, $options->ftp_root, JPATH_SITE), '/language/de-DE/de-DE.com_impressum.ini');
+					$return = $return && $ftp->delete($file);
+					
+					$file = JPath::clean(str_replace(JPATH_CONFIGURATION, $options->ftp_root, JPATH_SITE), '/language/en-GB/en-GB.com_impressum.ini');
+					$return = $return && $ftp->delete($file);
+							
+					$ftp->quit();
+				} 
+				else
+				{
+					ob_start();
+					$return = JFile::delete($path . '/language/de-DE/de-DE.com_impressum.ini');
+					// @TODO: check whether files exists or not
+					$return = $return && JFile::delete($path . '/language/de-DE/de-DE.com_impressum.sys.ini');
+					
+					$return = $return && JFile::delete($path . '/language/en-GB/en-GB.com_impressum.ini');
+					$return = $return && JFile::delete($path . '/language/en-GB/en-GB.com_impressum.sys.ini');
+					
+					$return = $return && JFile::delete(JPATH_SITE . '/language/de-DE/de-DE.com_impressum.ini');
+					
+					$return = $return && JFile::delete(JPATH_SITE . '/language/en-GB/en-GB.com_impressum.ini');
+					
+				ob_end_clean();
+				}
+				
+				$db = JFactory::getDbo();
+				$db->setQuery("ALTER TABLE `#__impressum` ADD `templateemail` varchar(255) NOT NULL default '';"
+					. " ALTER TABLE `#__impressum` CHANGE `aktiv` `default` tinyint NOT NULL default 0;");
+				$db->query();
+			} */
+		}
+		else
+		{
+			$rel = $this->release;
+		}
+		
+		echo '<p>' . JText::_('COM_IMPRESSUM_PREFLIGHT_' . $type . '_TEXT') . '</p>';
+	}
  
 	/**
 	 * Method to run after an install/update/uninstall method.
@@ -99,4 +213,35 @@ class com_impressumInstallerScript
 // 	{
 // 		echo '<p>' . JText::_('COM_IMPRESSUM_POSTFLIGHT_' . $type . '_TEXT') . '</p>';
 // 	}
+
+	/**
+	 * Method to get the params of the installed version.
+	 * 
+	 * @author	mgebhardt
+	 * @since	3.0.1
+	 */
+// Does not work. Use getOldVersion to get the version of the installed impressum.
+// 	function getParams()
+// 	{
+// 		$db = JFactory::getDbo();
+// 		$db->setQuery('SELECT manifest_cache FROM #__extensions WHERE name = "com_impressum"');
+// 		$params = new JRegistry();
+// 		$params->loadString($db->loadResult());
+// 		return $params;
+// 	}
+	
+	/**
+	 * Method to get the version of the install impressum.
+	 * 
+	 * @author	mgebhardt
+	 * @since	3.0.1
+	 * @return	string	The version of the installt impressum.
+	 */
+	function getOldVersion()
+	{
+		$db = JFactory::getDbo();
+		$db->setQuery('SELECT manifest_cache FROM #__extensions WHERE name = "com_impressum"');
+		$manifest = json_decode( $db->loadResult(), true );
+		return $manifest['version'];
+	}
 }
