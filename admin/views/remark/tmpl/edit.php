0a0352ec95b3aec081030de382b891e82e8a095b
Mathias Gebhardt (LT) <gebhardtmathias@aol.com>
2012-11-19 18:20:02 +0100

    [#7] Add user hint to remark's edit view for %%SiteName%% place holder.

--- admin/views/remark/tmpl/edit.php
+++ admin/views/remark/tmpl/edit.php
@@ -14,7 +14,7 @@ JHtml::_('behavior.formvalidation');
 $params = $this->form->getFieldsets('params');
 ?>
 <form action="<?php echo JRoute::_('index.php?option=com_imprint&view=remark&layout=edit&id='.(int) $this->item->id); ?>" method="post" name="adminForm" id="imprint-form" class="form-validate">
-	<div class="width-100 fltlft">
+	<div class="width-80 fltlft">
 		<fieldset class="adminform">
 			<legend><?php echo JText::_('JDETAILS'); ?></legend>
 			<ul class="adminformlist">
@@ -26,27 +26,9 @@ $params = $this->form->getFieldsets('params');
 			<?php echo $this->form->getInput('text'); ?>
 		</fieldset>
 	</div>
-<!--
-	<div class="width-40 fltrt">
-		<?php echo JHtml::_('sliders.start', 'imprint-slider'); ?>
-
-<?php foreach ($params as $name => $fieldset): ?>
-		<?php echo JHtml::_('sliders.panel', JText::_($fieldset->label), $name.'-params');?>
-	<?php if (isset($fieldset->description) && trim($fieldset->description)): ?>
-		<p class="tip"><?php echo $this->escape(JText::_($fieldset->description));?></p>
-	<?php endif;?>
-		<fieldset class="panelform" >
-			<ul class="adminformlist">
-	<?php foreach ($this->form->getFieldset($name) as $field) : ?>
-				<li><?php echo $field->label; ?><?php echo $field->input; ?></li>
-	<?php endforeach; ?>
-			</ul>
-		</fieldset>
-<?php endforeach; ?>
-
-		<?php echo JHtml::_('sliders.end'); ?>
+	<div class="width-20 fltrt">
+		<?php echo JText::_('COM_IMPRINT_REMARK_TEXT_HINTS')?>
 	</div>
--->
 	<div>
 		<input type="hidden" name="task" value="" />
 		<?php echo JHtml::_('form.token'); ?>
